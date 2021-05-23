0f8d26392b9a35c5eb4fabad9b2d05e55a7a4e20
CodeHut-coder <84077036+CodeHut-coder@users.noreply.github.com>
2021-05-23 12:23:46 +0530

    Create while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+#how you can do it with while loop 
+
+#input number
+n=int(input("enter num to added:"))
+
+#sum
+sum=0
+#updating
+while(n>0):
+  sum+=n
+  n-=1
+#output
+print("The number is:", sum)
